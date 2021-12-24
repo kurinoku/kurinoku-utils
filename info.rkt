abd54ead82916f03948f61f62219f8a3730a3a8b
kurinoku <31811692+kurinoku@users.noreply.github.com>
2021-12-24 00:05:24 -0300

    fix undeclared dependency problem

--- info.rkt
+++ info.rkt
@@ -1,6 +1,8 @@
 #lang info
 (define collection "kurinoku")
-(define deps '("base" "sugar" "static-rename"))
+(define deps '("db-lib"
+               "gui-lib"
+               "base" "sugar" "static-rename"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/kurinoku-utils.scrbl" ())))
 (define pkg-desc "Misc utils by and for kurinoku")
