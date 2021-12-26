2df2c7f9e51b4acdf0468f263231393f693e97a6
kurinoku <31811692+kurinoku@users.noreply.github.com>
2021-12-26 02:33:48 -0300

    add functions to optionally transform when conversion from and to sql-null fails

--- db.rkt
+++ db.rkt
@@ -1,15 +1,28 @@
 #lang racket/base
 
 (require racket/contract
+         racket/bool
          db)
 
 (provide (contract-out
           #:unprotected-submodule no-contract
           [query-user-version (-> connection? integer?)]
-          [set-user-version! (-> connection? integer? void?)]))
+          [set-user-version! (-> connection? integer? void?)]
+          [sql-null->false-or-apply (-> any/c (-> any/c any/c) any)]
+          [false->sql-null-or-apply (-> any/c (-> any/c any/c) any)]))
 
 (define (query-user-version conn)
   (query-value conn "PRAGMA user_version"))
 
 (define (set-user-version! conn new-version)
   (query-exec conn (format "PRAGMA user_version=~a" new-version)))
+
+(define (sql-null->false-or-apply v f)
+  (if (sql-null? v)
+      #f
+      (f v)))
+
+(define (false->sql-null-or-apply v f)
+  (if (false? v)
+      sql-null
+      (f v)))
